1b69722104f997ac5dd5488386725db57b2ea035
RAT1onal <ivan040203@gmail.com>
2025-12-25 18:48:38 +0300

    Add Lesson345

---
+++ Lesson3/sharer/src/main/java/ru/mirea/matveevir/sharer/ShareActivity.java
@@ -0,0 +1,17 @@
+package ru.mirea.matveevir.sharer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_share);
+
+    }
+}
---
+++ Lesson4/app/src/main/java/ru/mirea/matveevir/lesson4/MainActivity.java
@@ -0,0 +1,47 @@
+package ru.mirea.matveevir.lesson4;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.mirea.matveevir.lesson4.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(MainActivity.class.getSimpleName(),"Stop");
+                if (binding.btnStop.getText().toString().equals("■")) {
+                    binding.btnStop.setText("▶");
+                }else{
+                    binding.btnStop.setText("■");
+                }
+            }
+        });
+
+        binding.btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(MainActivity.class.getSimpleName(),"Next");
+                binding.tvTrack.setText("Zaz - Je veux");
+            }
+        });
+        binding.btnPrevios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(MainActivity.class.getSimpleName(),"Previos");
+                binding.tvTrack.setText("The Rapture - Echoes");
+            }
+        });
+    }
+}
